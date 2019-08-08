39f252f29fc8d78bc0cd9854828a364326f3da93
Harriet H-W <harriet.h@thoughtworks.com>
2019-08-08 16:48:43 +0100

    Updates

---
+++ precache-manifest.8639a4aa75a903ada1eb4fe54c1ea2c5.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "02e6b225ea4e1eac5bdf19e0145a2eb9",
+    "url": "/react-spa/index.html"
+  },
+  {
+    "revision": "21c55ad32e3cd7d37cb2",
+    "url": "/react-spa/static/css/2.22a7d4ef.chunk.css"
+  },
+  {
+    "revision": "21c55ad32e3cd7d37cb2",
+    "url": "/react-spa/static/js/2.d5f5099b.chunk.js"
+  },
+  {
+    "revision": "6060e0165f3fe61922c7",
+    "url": "/react-spa/static/js/main.abff0e32.chunk.js"
+  },
+  {
+    "revision": "3e306d59b7e4046b4373",
+    "url": "/react-spa/static/js/runtime~main.97fb65bb.js"
+  }
+]);
